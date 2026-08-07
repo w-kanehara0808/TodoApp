3a3ac98a811011ab83b3b2c39704a8031af90627
w_kanehara <w_kanehara@nexus-nt.co.jp>
2026-08-08 00:21:19 +0900

    例外処理・日付フォーマット変更を追加

--- TodoApp/src/main/Main.java
+++ TodoApp/src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
 import manager.TodoManager;
@@ -13,6 +15,9 @@ public class Main {
 
 		//2.Scannerを作る
 		Scanner scanner = new Scanner(System.in);
+
+		//日付フォーマット
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 
 		//3.whileループでメニューを繰り返す
 		while (true) {
@@ -39,26 +44,57 @@ public class Main {
 				//①タイトルを入力させる
 				System.out.println("タイトルを入力してください");
 				String title = scanner.nextLine();
-				//②期限を入力させる
-				System.out.println("期限を入力してください(例：2026-08-25) >");
-				LocalDate dueDate = LocalDate.parse(scanner.nextLine());
+
+				LocalDate dueDate = null;
+				boolean valid = false;
+
+				while (!valid) {
+					try {
+						//②期限を入力させる
+						System.out.println("期限を入力してください(例：2026-08-25) >");
+						dueDate = LocalDate.parse(scanner.nextLine(), formatter);
+						valid = true;
+
+					} catch (DateTimeParseException e) {
+						System.out.println("正しく入力してください");
+					}
+				}
 				manager.addTask(title, dueDate);
 			}
+
 			case 2 -> {
 				//IDを入力させる
 				System.out.println("DoenにするタスクのIDを入力してください >");
 				int id = scanner.nextInt();
 				manager.complete(id);
 			}
+
 			case 3 -> {
 				System.out.println("編集するタスクのIDを入力してください >");
 				//タスクを呼び出して編集する
 				int id = scanner.nextInt();
 				scanner.nextLine();
+
 				System.out.println("新しいタイトルを入力してください(変更しない場合はEnter) >");
 				String title = scanner.nextLine();
-				System.out.println("新しい期限を入力してください(例：2026-08-25) >");
-				LocalDate dueDate = LocalDate.parse(scanner.nextLine());
+
+				LocalDate dueDate = null;
+				boolean valid = false;
+				
+				while(!valid) {
+					try {
+						System.out.println("新しい期限を入力してください(変更しない場合はEnter) >");
+						String input = scanner.nextLine();
+						if(input.isEmpty()) {
+							valid = true;
+						}else {
+							dueDate = LocalDate.parse(input, formatter);
+							valid = true;
+						}
+					}catch(DateTimeParseException e) {
+						System.out.println("正しく入力してください");
+					}
+				}
 				manager.edit(id, title, dueDate);
 			}
 			case 4 -> {
